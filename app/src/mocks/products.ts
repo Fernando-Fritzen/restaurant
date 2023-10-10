1b0f0fd2106a28df2688bcbd81f734173061ff62
Fernando <fernandofritzen9@gmail.com>
2023-10-10 20:13:13 -0300

    app

---
+++ app/src/mocks/products.ts
@@ -0,0 +1,71 @@
+export const products = [
+  {
+    _id: "6372e040f52e37ef85fe2c5e",
+    name: "Pizza quatro queijos",
+    description: "Deliciosa pizza quatro queijos com borda simples",
+    imagePath: "1674520717693-quatro-queijos.png",
+    price: 40,
+    ingredients: [
+      {
+        name: "Mussarela",
+        icon: "🧀",
+        _id: "6372e040f52e37ef85fe2c5f"
+      },
+      {
+        name: "Parmesão",
+        icon: "🧀",
+        _id: "6372e040f52e37ef85fe2c60"
+      },
+      {
+        name: "Gouda",
+        icon: "🧀",
+        _id: "6372e040f52e37ef85fe2c61"
+      },
+      {
+        name: "Brie",
+        icon: "🧀",
+        _id: "6372e040f52e37ef85fe2c62"
+      }
+    ],
+  },
+  {
+    _id: "6372e276a381106c0f854cb3",
+    name: "Coca cola",
+    description: "Coca cola lata geladinha topzera",
+    imagePath: "1674521477046-coca-cola.png",
+    price: 7,
+    ingredients: [],
+  },
+  {
+    _id: "6372e276rhera381106c0f854cb3",
+    name: "Coca cola",
+    description: "Coca cola lata geladinha topzera",
+    imagePath: "1674521477046-coca-cola.png",
+    price: 7,
+    ingredients: [],
+  },
+  {
+    _id: "6372e276a3ehrerh1106c0f854cb3",
+    name: "Coca cola",
+    description: "Coca cola lata geladinha topzera",
+    imagePath: "1674521477046-coca-cola.png",
+    price: 7,
+    ingredients: [],
+  },
+  {
+    _id: "6372e276a38110wsgwerh6c0f854cb3",
+    name: "Coca cola",
+    description: "Coca cola lata geladinha topzera",
+    imagePath: "1674521477046-coca-cola.png",
+    price: 7,
+    ingredients: [],
+  },
+  {
+    _id: "ehne",
+    name: "Coca cola",
+    description: "Coca cola lata geladinha topzera",
+    imagePath: "1674521477046-coca-cola.png",
+    price: 7,
+    ingredients: [],
+  },
+];
