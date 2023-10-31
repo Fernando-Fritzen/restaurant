54f6d46751f56dfa338da71bc71193c5e0c8d2d5
Laércio Bubiak <49946934+LaercioMLB@users.noreply.github.com>
2023-10-31 18:56:11 -0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 export const io = new Server(server);
 
-mongoose.connect('mongodb://localhost:27017')
+mongoose.connect('mongodb://mongo:27017')
   .then(() => {
     const port = 3001;
 
